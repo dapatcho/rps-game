d461433e90e437e3cd0b5ea3610e087b435920a3
dapatcho <boulosmk@gmail.com>
2024-02-23 12:47:06 -0500

    Working towards incrementing and tracking wins.

--- rps.js
+++ rps.js
@@ -18,19 +18,32 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = caseChange(playerSelection);
 
   if (playerSelection === computerSelection) {
+    gameDraws = gameDraws++;
     return "It's a draw! You threw the same hands.";
   } else if (
     (playerSelection === "Rock" && computerSelection === "Scissors") ||
     (playerSelection === "Paper" && computerSelection === "Rock") ||
     (playerSelection === "Scissors" && computerSelection == "Paper")
   ) {
+    playerWins = playerWins++;
     return `You win! You chose ${playerSelection} and the computer chose ${computerSelection}.`;
   } else {
+    computerWins = computerWins++;
     return `You lose! You chose ${playerSelection} and the computer chose ${computerSelection}.`;
   }
 }
-const playerSelection = "ROCK";
-const computerSelection = getComputerChoice();
-caseChange(playerSelection);
-console.log(playRound(playerSelection, computerSelection));
 
+function playGame() {
+  let playerWins = 0;
+  let computerWins = 0;
+  let gameDraws = 0;
+  for (var i = 1; i <= 5; i++) {
+    const playerSelection = "ROCK";
+    const computerSelection = getComputerChoice();
+    caseChange(playerSelection);
+    caseChange(computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
+
+console.log(playGame());
