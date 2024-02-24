2bf252d43b4a64ef8e88a03dd5d91e06eae36f09
Michael Boulos <boulosmk@gmail.com>
2024-02-24 11:32:47 -0500

    Updates

--- rps.js
+++ rps.js
@@ -4,22 +4,21 @@ function getComputerChoice() {
   const computerOptions = ["Rock", "Paper", "Scissors"];
   const computerNumber = Math.floor(Math.random() * 3);
   const computerChoice = computerOptions[computerNumber];
-  // console.log(computerNumber);
-  // console.log(computerOptions);
-  // console.log(computerChoice);
   return computerChoice;
 }
 
-// if
-
-// function playRound(playerSelection, computerSelection) {
-// if (playerSelection === computerSelection) {
-//   return "It's a draw! You threw the same hands."
-// }
-// else if (playerSelection)
-// }
 const playerSelection = "ROCK";
 const computerSelection = getComputerChoice();
+
+if
+
+function playRound(playerSelection, computerSelection) {
+if (playerSelection === computerSelection) {
+  return "It's a draw! You threw the same hands."
+}
+else if (playerSelection === '')
+}
+
 
 function caseChange(string) {
   return string[0].toUpperCase() + string.slice(1).toLowerCase();
